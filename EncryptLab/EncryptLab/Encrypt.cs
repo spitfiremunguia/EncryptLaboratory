82603a5402071778fae1a816cdeb4217acecbdcb
Mynor Xico <mynorottoniel.xico2@gmail.com>
2017-10-05 20:08:51 -0600

    Auto stash before merge of "Key_Generator" and "origin/Key_Generator"
    - Merge entre la codificación del mensaje y generación de llave

--- EncryptLab/EncryptLab/Encrypt.cs
+++ EncryptLab/EncryptLab/Encrypt.cs
@@ -8,24 +8,11 @@ namespace EncryptLab
 {
     public class Encrypt
     {
-
         private static string key = "133457799BBCDFF1";
-        private string InitialPermutation(string M)
+
+        public string FFunction(string RightHalf, string Key)
         {
-            int[,] Ip = new int[,]{
-            {58,50,42,34,26,18,10,2},
-            {60,52,44,36,28,20,12,4},
-            {62,54,46,38,30,22,14,6},
-            {64,56,48,40,32,24,16,8},
-            {57,49,41,33,25,17,9,1},
-            {59,51,43,35,27,19,11,3},
-            {61,53,45,37,29,21,13,5},
-            {63,55,47,39,31,23,15,7}};
-            return Utilities.permutate(Ip,M);
-        }
-        private static string Ffunction(string right)
-        {
-            int[,] EbitTable = new int[,]{
+            int[,] E_Matrix = new int[,]{
             {32,1,2,3,4,5},
             {4,5,6,7,8,9},
             {8,9,10,11,12,13},
@@ -34,9 +21,50 @@ namespace EncryptLab
             {20,21,22,23,24,25},
             {24,25,26,27,28,29},
             {28,29,30,31,32,1}};
-            string firstPermutate= Utilities.permutate(EbitTable, right);
 
+            int[,] P_Matrix = new int[,]{
+            {16,7,20,21},
+            {29,12,28,17},
+            {1,15,23,26},
+            {5,18,31,10},
+            {2,8,24,14},
+            {19,13,30,6},
+            {22,11,4,25}};
+
+            string E = Utilities.permutate(E_Matrix,RightHalf);
+            string xored = XOR(E, Key);
+
+            List<string> addresses = new List<string>();
+            for (int i = 0; i < E.Length; i += 6)
+            {
+                addresses.Add(E.Substring(i, i + 5)); // we have 8 groups, 8 bits each one
+            }
+            StringBuilder output = new StringBuilder();
+            foreach (string s in addresses)
+            {
+                output.Append(SBoxFunction(s));
+            }
+            return Utilities.permutate(P_Matrix, output.ToString());
         }
+
+        public string SBoxFunction(string s) {
+
+            int[,] S_Matrix = new int[,]{
+            {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
+            {0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
+            {4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
+            {15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}};
+
+            string strExtremos = s[0].ToString() + s[5].ToString();
+            string strInternos = s.Substring(1, 4);
+
+            int row = Convert.ToInt32(strExtremos, 2);
+            int column = Convert.ToInt32(strInternos, 2);
+            int number = S_Matrix[row, column];
+
+            return Convert.ToString(number, 2);
+        }
+
         private static string XOR(string subkey,string Fright)
         {
             string xor = string.Empty;
@@ -46,18 +74,39 @@ namespace EncryptLab
             }
             return xor;
         }
-        private static string FinalRight(string Ip)
+
+        public string EncodeData(string M)
         {
-            string firstleft = Ip.Substring(0, Ip.Length / 2);
-            string firstRigth = Ip.Substring(Ip.Length / 2);
-            var keys = KeyGenerator.GenerateKeys(key);
-            string f = Ffunction(firstRigth);
-            string firstXOR = XOR(firstleft, f);
-            for(int i=0; i<key.Length;i++)
+            int[,] InerseIP_Matrix = new int[,]{
+            {16,7,20,21},
+            {29,12,28,17},
+            {1,15,23,26},
+            {5,18,31,10},
+            {2,8,24,14},
+            {19,13,30,6},
+            {22,11,4,25}};
+
+            List<string> GeneratedKeys = KeyGenerator.GenerateKeys(key);
+
+            List<string> L = new List<string>();
+            List<string> R = new List<string>();
+
+            L.Add(M.Substring(0, M.Length / 2));
+            R.Add(M.Substring(M.Length / 2));
+
+            for (int i = 1; i < 16; i++)
             {
-
+                L.Add(R.Last());
+                R.Add(XOR(L.Last(), FFunction(R.Last(), GeneratedKeys[i])));
             }
-        }
 
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < L.Count; i++)
+            {
+                output.Append(R[i]);
+                output.Append(L[i]);
+            }
+            return Utilities.permutate(InerseIP_Matrix, output.ToString());
+        }          
     }
 }
